92dfc793185ac9e953d069f9d43d768e20402850
slobodaapl <slobodaapl@gmail.com>
2025-01-26 22:10:20 +0100

    Sync migrations

--- alembic/env.py
+++ alembic/env.py
@@ -4,19 +4,21 @@ if os.getenv("CI"):
     exit(0)
 
 from logging.config import fileConfig
-from typing import cast
+from typing import Any, cast
 
 from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from ai_api.database.postgres_client import Postgres
 from ai_api import settings
 from ai_api.orm import Base
+from ai_api.orm.gallery_embedding import GalleryEmbedding # type: ignore[unused-import]
+from ai_api.orm.image import ImageHash, Image # type: ignore[unused-import]
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.postgres.url.replace("+asyncpg", ""))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -28,14 +30,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-
-Postgres(url=settings.postgres.url)
 
 
 def run_migrations_offline() -> None:
@@ -50,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = os.environ.get("DATABASE_URL", "sqlite:///./api.db")  # TODO figure out config source
+    url = settings.postgres.url.replace("+asyncpg", "")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -69,8 +63,8 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    configuration: dict = cast(dict, config.get_section(config.config_ini_section))
-    configuration["sqlalchemy.url"] = settings.postgres.url
+    configuration: dict[str, Any] = cast(dict[str, Any], config.get_section(config.config_ini_section))
+    configuration["sqlalchemy.url"] = settings.postgres.url.replace("+asyncpg", "")
     connectable = engine_from_config(
         configuration, prefix="sqlalchemy.", poolclass=pool.NullPool,
     )
